debb416f3ac9d4eb6bf39adcdc3aa852f11d5638
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-08 14:26:01 -0300

    defining the Bean method

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,4 +1,21 @@
 package config;
 
+import main.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// we use the @Configuration annotation to define this class
+// as a Spring configuration class.
+@Configuration
 public class ProjectConfig {
+
+    // By adding the @Bean annotation, we instruct Spring to call this method
+    // when at context initialization and add the returned value to the context.
+    @Bean
+    Parrot parrot() {
+        var p = new Parrot();
+        p.setName("Koko");  // Set a name for the parrot we'll use later when we test the app.
+        return p;   // Spring adds to its context the Parrot instance returned by the method
+    }
+
 }
