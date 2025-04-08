843d793d5cae7d1010b84a96192e7d0702a27e9a
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-08 16:17:12 -0300

    using PostConstruct to manage the instance after its creation

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -2,12 +2,19 @@ package main;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 // by using the @Component annotation over the class, we instruct Spring
 // to create an instance of this class and add it to its context.
 @Component
 public class Parrot {
 
     private String name;
+
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
 
     // getters and setters
     public String getName() {
