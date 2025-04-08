78b510556751d8a80ce3c16733d16ac660142d53
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-08 15:46:37 -0300

    adding multiple beans of the same type to the Spring context

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package config;
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // we use the @Configuration annotation to define this class
 // as a Spring configuration class.
@@ -11,13 +12,29 @@ public class ProjectConfig {
 
     // By adding the @Bean annotation, we instruct Spring to call this method
     // when at context initialization and add the returned value to the context.
-    @Bean
-    Parrot parrot() {
+    @Bean(value = "koko")
+    Parrot parrot1() {
         var p = new Parrot();
         p.setName("Koko");  // Set a name for the parrot we'll use later when we test the app.
         return p;   // Spring adds to its context the Parrot instance returned by the method
     }
 
+    @Bean(name = "miki")    // Sets the name of the bean
+    @Primary    // Spring will select Miki by default, so you can refer the Parrot without the specific name
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");  // Sets the name of the parrot
+        return p;
+    }
+
+    @Bean("riki")    // Sets the name of the bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");  // Sets the name of the parrot
+        return p;
+    }
+
+    /*
     @Bean
     String hello() {    // adds the string "Hello" to the Spring context
         return "Hello";
@@ -27,5 +44,6 @@ public class ProjectConfig {
     Integer ten() {     // adds the integer 10 to the Spring context
         return 10;
     }
+    */
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,14 +11,14 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         // Gets a reference of a bean of type Parrot from the Spring context
-        Parrot p = context.getBean(Parrot.class);   //
+        Parrot p = context.getBean("parrot2", Parrot.class);   //
         System.out.println(p.getName());
 
-        String s = context.getBean(String.class);
-        System.out.println(s);
+//        String s = context.getBean(String.class);
+//        System.out.println(s);
 
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
+//        Integer n = context.getBean(Integer.class);
+//        System.out.println(n);
     }
 
 }
